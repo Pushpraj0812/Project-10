d3eb6dd9138acfe833ae44e83f689130de885fe1
Pushpraj0812 <167536056+Pushpraj0812@users.noreply.github.com>
2026-01-16 23:24:20 +0530

    16/01/2026

---
+++ ORSProject-10/src/main/java/com/rays/ctl/UserCtl.java
@@ -0,0 +1,22 @@
+package com.rays.ctl;
+
+import javax.validation.Valid;
+
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.rays.common.BaseCtl;
+import com.rays.common.ORSResponse;
+import com.rays.dao.UserDAOImpl;
+import com.rays.dto.UserDTO;
+import com.rays.form.UserForm;
+import com.rays.service.UserServiceInt;
+
+@RestController
+@RequestMapping(value = "User")
+public class UserCtl extends BaseCtl<UserForm, UserDTO, UserServiceInt> {
+
+}
---
+++ ORSProject-10/src/main/java/com/rays/service/UserServiceInt.java
@@ -0,0 +1,11 @@
+package com.rays.service;
+
+import java.util.List;
+
+import com.rays.common.BaseServiceInt;
+import com.rays.common.UserContext;
+import com.rays.dto.UserDTO;
+
+public interface UserServiceInt extends BaseServiceInt<UserDTO> {
+
+}
