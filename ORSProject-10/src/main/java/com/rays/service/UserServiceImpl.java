4c7944f3cc6abd78cb5d60335f2b27cd778136cd
Pushpraj0812 <167536056+Pushpraj0812@users.noreply.github.com>
2026-01-21 11:48:35 +0530

    21/01/2025

--- ORSProject-10/src/main/java/com/rays/service/UserServiceImpl.java
+++ ORSProject-10/src/main/java/com/rays/service/UserServiceImpl.java
@@ -1,5 +1,8 @@
 package com.rays.service;
 
+import java.sql.Timestamp;
+import java.util.Date;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Propagation;
@@ -14,4 +17,38 @@ import com.rays.dto.UserDTO;
 @Transactional
 public class UserServiceImpl extends BaseServiceImpl<UserDTO, UserDAOInt> implements UserServiceInt {
 
+	@Transactional(readOnly = true)
+	public UserDTO findByLoginId(String login, UserContext userContext) {
+		return baseDao.findByUniqueKey("loginId", login, userContext);
+	}
+
+	@Override
+	public UserDTO register(UserDTO dto, UserContext userContext) {
+
+		Long id = add(dto, userContext);
+
+		dto.setId(id);
+
+		return dto;
+	}
+
+	@Override
+	public UserDTO authenticate(String loginId, String password) {
+
+		UserDTO dto = findByLoginId(loginId, null);
+
+		if (dto != null) {
+			UserContext userContext = new UserContext(dto);
+			if (password.equals(dto.getPassword())) {
+				dto.setLastLogin(new Timestamp((new Date()).getTime()));
+				dto.setUnsucessfullLoginAttempt(0);
+				update(dto, userContext);
+				return dto;
+			} else {
+				dto.setUnsucessfullLoginAttempt(1 + dto.getUnsucessfullLoginAttempt());
+				update(dto, userContext);
+			}
+		}
+		return null;
+	}
 }
